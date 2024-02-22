999a95c998300083aa47a590f9c4608dc1bd813e
Tatianka-hk <t0681689150@gmail.com>
2024-02-22 20:10:39 +0100

    done 2 lab

--- lab1/src/file_datasource.py
+++ lab1/src/file_datasource.py
@@ -1,37 +1,30 @@
-# from csv import reader
-# from datetime import datetime
-# from domain.aggregated_data import AggregatedData
-
-# class FileDatasource:
-#   def __init__(self, accelerometer_filename: str, gps_filename: str) -> None:
-#     pass
-#   def read(self) -> AggregatedData:
-#     """Метод повертає дані отримані з датчиків"""
-#   def startReading(self, *args, **kwargs):
-#     """Метод повинен викликатись перед початком читання даних"""
-#   def stopReading(self, *args, **kwargs):
-#     """Метод повинен викликатись для закінчення читання даних"""
 from csv import reader
 from datetime import datetime
 from domain.accelerometer import Accelerometer
 from domain.gps import Gps
 from domain.aggregated_data import AggregatedData
+from domain.parking import Parking
 import config
-
+import pandas as pd
 
 class FileDatasource:
     def __init__(
         self,
         accelerometer_filename: str,
         gps_filename: str,
+        parking_filename:str,
     ) -> None:
         pass
 
     def read(self) -> AggregatedData:
         """Метод повертає дані отримані з датчиків"""
+        accelerometer_data = Accelerometer(1, 2, 3)
+        gps_data = Gps(4, 5)
+        parking_instance = Parking(6, gps_data)
         return AggregatedData(
-            Accelerometer(1, 2, 3),
-            Gps(4, 5),
+            accelerometer_data,
+            gps_data,
+            parking_instance,
             datetime.now(),
             config.USER_ID,
         )
